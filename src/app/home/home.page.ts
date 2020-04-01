fd253a784fa83885e0b3f41eb6f55b31bf2d4250
Yashveer Ramparsad <codenameyash@gmail.com>
2020-04-01 20:19:42 +0200

    cards are dynamic and added dynamic messages

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import {Crud} from '../shared/services/crud'
-import { RouterModule, Routes } from '@angular/router';
 import {InventoryPage} from './inventory/inventory.page'
 
 @Component({
@@ -12,25 +10,7 @@ export class HomePage {
   
   rootPage: any = InventoryPage;
 
- 
-
-  clients:any;
-  clientsId:string;
-  clientsSense:string;
-  clientsSenseVal:number;
-
-  constructor(private crud_service:Crud) {}
-  ngOnInit(){
-
-    this.crud_service.read_data('IpGvAMwKPTwCcPZJQahA').subscribe(data => {
-
-      console.log(data.data());
-              id:data.id,
-
-              this.clientsId= data.data()['Client'],
-              this.clientsSense=data.data()['Sensor01'],
-              this.clientsSenseVal = data.data()['Sensor01Val']
-    });
-    
+  constructor() {}
+  ngOnInit(){        
   }
 }
--- src/app/home/inventory/inventory.page.ts
+++ src/app/home/inventory/inventory.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Crud} from '../../shared/services/crud'
 
 @Component({
   selector: 'app-inventory',
@@ -7,9 +8,90 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InventoryPage implements OnInit {
 
-  constructor() { }
+  clients:any;
+  clientsId:string;
+  clientsSense:string;
+  clientsSenseVal:number;
+  clientsVolume:number;
+
+  dynamicVolume:number;
+
+  dynamicColor:string
+
+  dynamicMessage:string;
+
+  constructor(private crud_service:Crud) { 
+
+  }
 
   ngOnInit() {
+
+    this.crud_service.read_data('IpGvAMwKPTwCcPZJQahA').subscribe(data => {
+
+      console.log(data.data());
+              id:data.id,
+
+              this.clientsId= data.data()['Client'],
+              this.clientsSense=data.data()['Sensor01'],
+              this.clientsSenseVal = data.data()['Sense01Val']
+              this.clientsVolume = data.data()['Volume']
+              console.log(this.clientsSenseVal);
+              console.log(this.clientsVolume);
+              this.calculate();
+              
+    });
+    
+
+  }
+
+  doRefresh(event) {
+    console.log('Begin async operation');
+    this.read();
+    
+    setTimeout(() => {
+      console.log('Async operation has ended');
+      event.target.complete();
+    }, 2000);
+  }
+
+  calculate()
+  {
+    this.dynamicVolume = (this.clientsSenseVal / this.clientsVolume) * 100
+
+    console.log(this.dynamicVolume);
+    if (this.dynamicVolume >= 65)
+    {
+      this.dynamicColor = "success"
+      this.dynamicMessage = "Uber Cool, looks like we are A OK for use"
+    }
+    else if ((this.dynamicVolume > 30) && (this.dynamicVolume < 65))
+    {
+      this.dynamicColor = "warning"
+      this.dynamicMessage = "Hmmmm ... we are still fine here, onward..."
+    }
+    else
+    {
+      this.dynamicColor = "danger"
+      this.dynamicMessage = "Arrrrrrghhhh, its time to fill up, its running empty"
+    }
+  }
+
+  read()
+  {
+    this.crud_service.read_data('IpGvAMwKPTwCcPZJQahA').subscribe(data => {
+
+      console.log(data.data());
+              id:data.id,
+
+              this.clientsId= data.data()['Client'],
+              this.clientsSense=data.data()['Sensor01'],
+              this.clientsSenseVal = data.data()['Sense01Val']
+              this.clientsVolume = data.data()['Volume']
+              console.log(this.clientsSenseVal);
+              console.log(this.clientsVolume);
+              this.calculate();
+              
+    });
   }
 
 }
