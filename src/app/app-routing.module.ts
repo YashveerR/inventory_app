836071e021575afce835957855081bed27dcaa1c
Yashveer Ramparsad <codenameyash@gmail.com>
2021-03-01 18:30:06 +0200

    Updated the inventory pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,43 +1,69 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 
 // Import canActivate guard services
 import { AuthGuard } from "./shared/guard/auth.guard";
 import { SecureInnerPagesGuard } from "../app/shared/guard/secure-inner-pages.guard.ts.guard";
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'home', loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)},
+  { path: "", redirectTo: "login", pathMatch: "full" },
   {
-    path: 'login',
-    loadChildren: () => import('./pages/login/login.module').then( m => m.LoginPageModule)
+    path: "home",
+    loadChildren: () =>
+      import("./home/home.module").then((m) => m.HomePageModule),
   },
   {
-    path: 'forgotpass',
-    loadChildren: () => import('./pages/forgotpass/forgotpass.module').then( m => m.ForgotpassPageModule)
+    path: "login",
+    loadChildren: () =>
+      import("./pages/login/login.module").then((m) => m.LoginPageModule),
   },
   {
-    path: 'home/settings',
-    loadChildren: () => import('./home/settings/settings.module').then( m => m.SettingsPageModule), 
+    path: "forgotpass",
+    loadChildren: () =>
+      import("./pages/forgotpass/forgotpass.module").then(
+        (m) => m.ForgotpassPageModule
+      ),
   },
   {
-    path: 'home/inventory',
-    loadChildren: () => import('./home/inventory/inventory.module').then( m => m.InventoryPageModule), 
+    path: "home/settings",
+    loadChildren: () =>
+      import("./home/settings/settings.module").then(
+        (m) => m.SettingsPageModule
+      ),
   },
   {
-    path: 'register',
-    loadChildren: () => import('./register/register.module').then( m => m.RegisterPageModule)
+    path: "home/inventory",
+    loadChildren: () =>
+      import("./home/inventory/inventory.module").then(
+        (m) => m.InventoryPageModule
+      ),
   },
   {
-    path: 'home/inventory/chart/:name',
-    loadChildren: () => import('./home/chart/chart.module').then( m => m.ChartPageModule)
-  }
+    path: "register",
+    loadChildren: () =>
+      import("./register/register.module").then((m) => m.RegisterPageModule),
+  },
+  {
+    path: "home/inventory/chart/:name",
+    loadChildren: () =>
+      import("./home/chart/chart.module").then((m) => m.ChartPageModule),
+  },
+  {
+    path: "settings",
+    redirectTo: "home/settings",
+    pathMatch: "full",
+  },
+  {
+    path: "inventory",
+    redirectTo: "home/inventory",
+    pathMatch: "full",
+  },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
   ],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/home/inventory/inventory.page.ts
+++ src/app/home/inventory/inventory.page.ts
@@ -1,145 +1,139 @@
-import { Component, OnInit } from '@angular/core';
-import {Crud} from '../../shared/services/crud'
-import {devices} from '../../shared/services/devices'
-import {device_data} from '../../shared/services/devicedata'
+import { Component, OnInit } from "@angular/core";
+import { Crud } from "../../shared/services/crud";
+import { devices } from "../../shared/services/devices";
+import { device_data } from "../../shared/services/devicedata";
 import { AngularFireAuth } from "@angular/fire/auth";
-import * as firebase from 'firebase/app';
-import 'firebase/auth';
-import {AngularFirestore} from '@angular/fire/firestore'
-import { firestore } from 'firebase/app';
-import { first } from 'rxjs/operators';
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import { AngularFirestore } from "@angular/fire/firestore";
+import { firestore } from "firebase/app";
+import { first } from "rxjs/operators";
 
 @Component({
-  selector: 'app-inventory',
-  templateUrl: './inventory.page.html',
-  styleUrls: ['./inventory.page.scss'],
+  selector: "app-inventory",
+  templateUrl: "./inventory.page.html",
+  styleUrls: ["./inventory.page.scss"],
 })
 export class InventoryPage implements OnInit {
+  public dev_data_arr: device_data[] = [];
 
-  public dev_data_arr: device_data[]=[];  
+  current_user: string;
+  clientsId: string;
+  clientsSense: string;
+  clientsSenseVal: number;
+  clientsVolume: number;
 
-  current_user:string;
-  clientsId:string;
-  clientsSense:string;
-  clientsSenseVal:number;
-  clientsVolume:number;
+  dynamicVolume: number;
 
-  dynamicVolume:number;
+  dynamicColor: string;
 
-  dynamicColor:string
-
-  dynamicMessage:string;
+  dynamicMessage: string;
 
   devices_list: devices[];
   userdata: any;
-  data_here:boolean = false;
+  data_here: boolean = false;
+  emtpy_device: boolean = false;
 
   //ret_val:any;
 
   constructor(
-    private crud_service:Crud, 
+    private crud_service: Crud,
     private firestore: AngularFirestore,
-    public afAuths: AngularFireAuth) { 
-    var objct:any[]=[];        
+    public afAuths: AngularFireAuth
+  ) {
+    var objct: any[] = [];
   }
 
   isLoggedIn() {
     return this.afAuths.authState.pipe(first()).toPromise();
- }
+  }
 
- async doSomething() {
-  const user = await this.isLoggedIn()
+  async doSomething() {
+    const user = await this.isLoggedIn();
 
-  if (user) {
-    this.data_here = false;
-    this.read();
-    //this.calculate();     
-    
-  } else {
-    // do something else
- }
-}
- 
-  ngOnInit() { 
+    if (user) {
+      this.data_here = false;
+      this.read();
+      //this.calculate();
+    } else {
+      // do something else
+    }
+  }
 
+  ngOnInit() {
     this.doSomething();
   }
 
   doRefresh(event) {
-    this.data_here = false;   //set flag to draw the blocks to false
-    console.log('Begin async operation');
+    this.data_here = false; //set flag to draw the blocks to false
+    console.log("Begin async operation");
     this.devices_list = []; //empty device list
-    this.devices_list.length=0; //empty the
+    this.devices_list.length = 0; //empty the
     this.dev_data_arr.length = 0;
     this.read();
-   
-      
-      setTimeout(() => {     
-        //this.calculate();     
-        //this.data_here = true;      
-        console.log('Async operation has ended');
-        event.target.complete();
-      }, 2000);
 
-
+    setTimeout(() => {
+      //this.calculate();
+      //this.data_here = true;
+      console.log("Async operation has ended");
+      event.target.complete();
+    }, 2000);
   }
 
-  calculate()
-  {
-
-    this.dev_data_arr.forEach((value)=>{
-        value.dynamicVolume = (value.clientsSenseVal / value.clientsVolume) * 100
-        if (value.dynamicVolume >= 65)
-        {
-          value.dynamicColor = "success"
-          value.dynamicMessage = "Uber Cool, looks like we are A OK for use"
-        }
-        else if ((value.dynamicVolume > 30) && (value.dynamicVolume < 65))
-        {
-          value.dynamicColor = "warning"
-          value.dynamicMessage = "Hmmmm ... we are still fine here, onward..."
-        }
-        else
-        {
-          value.dynamicColor = "danger"
-          value.dynamicMessage = "Arrrrrrghhhh, its time to fill up, its running empty"
-        }
-
+  calculate() {
+    this.dev_data_arr.forEach((value) => {
+      value.dynamicVolume = (value.clientsSenseVal / value.clientsVolume) * 100;
+      if (value.dynamicVolume >= 65) {
+        value.dynamicColor = "success";
+        value.dynamicMessage = "Uber Cool, looks like we are A OK for use";
+      } else if (value.dynamicVolume > 30 && value.dynamicVolume < 65) {
+        value.dynamicColor = "warning";
+        value.dynamicMessage = "Hmmmm ... we are still fine here, onward...";
+      } else {
+        value.dynamicColor = "danger";
+        value.dynamicMessage =
+          "Arrrrrrghhhh, its time to fill up, its running empty";
+      }
     });
   }
 
-  read()
-  {
-    this.userdata = JSON.parse(localStorage.getItem('user'));
+  read() {
+    try {
+      firebase.auth().onAuthStateChanged((user: firebase.User) => {
+        if (user) {
+          this.userdata = JSON.parse(localStorage.getItem("user"));
 
-     this.crud_service.read_linked_devices(this.userdata["uid"]).subscribe(data => {
-      
-      
-      this.devices_list = data.data()['linked_devices']
-      console.log("Devices list length",this.devices_list.length)
-      this.devices_list.forEach((value)=> {
-        
-        this.crud_service.read_data(value['id']).subscribe(linkdata => {
-          
-                    console.log("subscribe in loop ",linkdata.data())
-                    var temp:device_data={clientsId: linkdata.data()['Client'],
-                                          clientsSense: linkdata.data()['Sense01'],
-                                          clientsSenseVal: linkdata.data()['Sense01Val'],
-                                          clientsVolume: linkdata.data()['Volume'],
-                                          dynamicColor: "",
-                                          dynamicMessage:"",
-                                          dynamicVolume:25 
-                                        };
-                    this.dev_data_arr.push(temp); 
-                    this.calculate();
-                    this.data_here = true;   
+          this.crud_service
+            .read_linked_devices(this.userdata["uid"])
+            .subscribe((data) => {
+              console.log();
+              if (data.data()["linked_devices"] != null) {
+                this.devices_list = data.data()["linked_devices"];
 
-        });
+                this.devices_list.forEach((value) => {
+                  this.crud_service
+                    .read_data(value["id"])
+                    .subscribe((linkdata) => {
+                      var temp: device_data = {
+                        clientsId: linkdata.data()["Client"],
+                        clientsSense: linkdata.data()["Sense01"],
+                        clientsSenseVal: linkdata.data()["Sense01Val"],
+                        clientsVolume: linkdata.data()["Volume"],
+                        dynamicColor: "",
+                        dynamicMessage: "",
+                        dynamicVolume: 25,
+                      };
+                      this.dev_data_arr.push(temp);
+                      this.calculate();
+                      this.data_here = true;
+                    });
+                });
+              } else {
+                this.emtpy_device = true;
+              }
+            });
+        }
       });
-
-    })
-   
+    } catch {}
   }
-
-
 }
